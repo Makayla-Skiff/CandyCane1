b089ef436a3be2e68d614ab4c0ce442415d65fac
Makayla-Skiff <54339097+Makayla-Skiff@users.noreply.github.com>
2019-10-22 13:27:35 -0500

    Add files via upload

---
+++ Candies.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyCaneLane1._1.Data
+{
+    public class Candies
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int CandyID { get; set; }
+        public string CandyName { get; set; }
+        public int Price { get; set; }
+        public bool? ApprPos { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+
+
+
+        public int StoreID { get; set; }
+        [ForeignKey("StoreID")]
+        public Stores Store { get; set; }
+
+
+        public int OrderID { get; set; }
+        [ForeignKey("OrderID")]
+        public Orders Order { get; set; }
+    }
+}
---
+++ Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CandyCaneLane1._1.Data;
+
+namespace CandyCaneLane1._1.Pages.Candy
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CandyCaneLane1._1.Data.ApplicationDbContext _context;
+
+        public CreateModel(CandyCaneLane1._1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["OrderID"] = new SelectList(_context.Set<Orders>(), "OderID", "OderID");
+        ViewData["StoreID"] = new SelectList(_context.Stores, "StoreID", "StoreID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Candies Candies { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Candies.Add(Candies);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
